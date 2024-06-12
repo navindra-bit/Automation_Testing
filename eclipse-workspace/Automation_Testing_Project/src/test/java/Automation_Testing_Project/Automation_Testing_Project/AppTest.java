f97ecf86a6f0461506ebc83e2151d273c79f17f9
Navindra <navindra063@gmail.com>
2024-06-12 16:42:09 +0530

    First commit

---
+++ eclipse-workspace/Automation_Testing_Project/src/test/java/Automation_Testing_Project/Automation_Testing_Project/AppTest.java
@@ -0,0 +1,20 @@
+package Automation_Testing_Project.Automation_Testing_Project;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
