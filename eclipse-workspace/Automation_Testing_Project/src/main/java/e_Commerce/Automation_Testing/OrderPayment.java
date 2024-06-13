f455e71b055ea22dd407cd37dac53702dc9ac5c8
Navindra <navindra063@gmail.com>
2024-06-13 16:33:38 +0530

    Orderpayment update

--- eclipse-workspace/Automation_Testing_Project/src/main/java/e_Commerce/Automation_Testing/OrderPayment.java
+++ eclipse-workspace/Automation_Testing_Project/src/main/java/e_Commerce/Automation_Testing/OrderPayment.java
@@ -8,17 +8,29 @@ public class OrderPayment {
      
 	WebDriver driver = null;
 	By editshippingdetails = By.xpath("//a[normalize-space()='Edit shipping details']");
+	By methodSafepay = By.cssSelector("img[alt='Safepay']");
+	By methodmanualpayBy = By.cssSelector("img[alt='Master credit']");
 	By paysafepay = By.xpath("//button[@id='pay_now_btn_SAFEPAY']");
-	By payManualpay = By.xpath("//button[@id='pay_now_btn_ManualPayment']");
-    
+	By paymanualpay = By.xpath("//button[@id='pay_now_btn_ManualPayment']");
+	By next = By.id("next_btn");
 	
 	public WebElement Editshippingdetails() {
 		return driver.findElement(editshippingdetails);
     }
+	public WebElement MethodSafepay() {
+		return driver.findElement(methodSafepay);
+    }
+	public WebElement MethodmanualpayBy() {
+		return driver.findElement(methodmanualpayBy);
+    }
 	public WebElement Paysafepay() {
 		return driver.findElement(paysafepay);
 	}
-	public WebElement PayManualpay() {
-		return driver.findElement(payManualpay);
+	public WebElement Paymanualpay() {
+		return driver.findElement(paymanualpay);
     }
+	public WebElement Next() {
+		return driver.findElement(next);
+	}
+	
 }
