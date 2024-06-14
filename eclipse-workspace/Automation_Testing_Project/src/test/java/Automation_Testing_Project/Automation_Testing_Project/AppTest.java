5d9f8995590274f1dbc12005d6f7ae5ff299eded
CosmicZero <84118384+navindra-bit@users.noreply.github.com>
2024-06-14 11:04:40 +0530

    Revert "Screenshot feature"

--- eclipse-workspace/Automation_Testing_Project/src/test/java/Automation_Testing_Project/Automation_Testing_Project/AppTest.java
+++ eclipse-workspace/Automation_Testing_Project/src/test/java/Automation_Testing_Project/Automation_Testing_Project/AppTest.java
@@ -2,51 +2,29 @@ package Automation_Testing_Project.Automation_Testing_Project;
 
 import static org.junit.Assert.assertTrue;
 
-import java.time.Duration;
+import java.sql.Driver;
 
-import org.openqa.selenium.WebDriver;
+import org.junit.Test;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
 
-import com.utility.functions.Screenshot;
-
-public class AppTest {
-
-	WebDriver driver;
-
-	@BeforeClass
-	public void setup() {
-		driver = new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
-		driver.manage().window().maximize();
-		driver.get("https://www.advantageonlineshopping.com/");
-	}
-
-//	======================== Demo to use Screenshot utility class =============================
-	@Test
-	public void testHomePage() {
-
-		assertTrue(driver.getTitle().contains("Advantage Shopping"));
-		Screenshot.takeScreenshot(driver, "test home page title");
-	}
-
-//	======================== Demo to use Screenshot utility class =============================
-
-	@Test
-	public void shouldAnswerWithTrue() {
-		assertTrue(true);
-	}
-
-	@AfterClass
-	public void teardown() {
-		try {
-			Thread.sleep(5000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		driver.quit();
-	}
-
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+    
+    @BeforeMethod
+    public void advantageonlineshopping() {
+    	Driver driver = (Driver) new ChromeDriver();
+    	
+    }
 }
